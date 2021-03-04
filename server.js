82b8d84304dc5d781ae9a7bd8fae26e1c2d4da92
Monica Hill <monica.r.hill@gmail.com>
2021-03-03 19:42:32 -0500

    constructing server.js

--- server.js
+++ server.js
@@ -59,6 +59,25 @@ inquirer
 
 questions();
 
+//User is able to view all departments currently in database 
+const viewAllDepartments = () => {
+    connection.query("SELECT * FROM department", (err, answer) => {
+        console.log("\n Departments retrieved from database \n");
+        console.table(answer);
+    });
+    questions();
+}
+
+//User is able to view all employee roles currently in database
+const viewAllRoles = () => {
+    connection.query("SELECT * FROM role", (err,answer) => {
+        console.log("\n Roles Retrieved from Database \n");
+        console.table(answer);
+    });
+    questions();
+
+}
+
 //User is able to view all employees currently in db
 
 const viewAllEmployees = () => {
@@ -71,6 +90,109 @@ const viewAllEmployees = () => {
     questions();
 }
 
-//User is able to view all roles currently in db
+//User is able to add a new employee
+const addEmployee = () =>
+    inquirer
+    .prompt ([
+       {
+        type: "input",
+        message: "Enter employee's first name.",
+        name: "firstname"
+       },
+       {
+        type: "input",
+        message: "Enter employee's last name.",
+        name: "lastname"
+       }
 
-//User is able to view all departments in db
+    ])
+    .then((answer) => {
+        connnection.query =
+        "INSERT INTO employee SET ?",
+        {
+            first_name: answer.firstname,
+            last_name: answer.lastname,
+            role_id: null,
+            manager_id: null
+        },
+        (err, answer) => {
+            if (err) {
+                throw err;
+            }
+            console.table(answer);
+        }
+    });
+    questions();
+
+};
+
+
+//User is able to grab employees and update their role
+const updateEmployee = () => {
+    let allemp = [];
+    connection.query("SELECT * FROM employee", (err,answer) => {
+        for (let i = 0; i < answer.length; i++) {
+            let employeeString =
+              answer[i].id + " " + answer[i].first_name + " " + answer [i].last.name;
+              allemp.push(employeeString);   
+        }
+        inquirer
+        .prompt ([
+        {
+            type: "list",
+            name: "updateEmployRole",
+            message: "Select an employee to update",
+            choices: allemp
+        },
+        {
+            type: "list",
+            name: "newrole",
+            message: "Update employees role",
+            choices: ["manager", "employee"],
+        },
+        ])
+        .then((answer) => {
+            console.log ("update processing", answer);
+            const updateID = {};
+            updateID.employeeId = parseInt(answer.updateEmployRole.split(" ") [0]);
+            if (answer.newrole === "manager") {
+                updateID.role_id = 1;
+            } else if (answer.newrole === "employee") {
+                updateID.role_id = 2;
+            }
+            connection.query(
+                "UPDATE employee SET role_id = ? WHERE id = ?",
+                [updateID.role_id, updateID.employeeId],
+                (err, data) => {
+                    questions();
+                }
+            );
+
+        });
+    });
+}
+
+//User is able to add a new departments in db
+const addDepartment = () => {
+    inquirer
+    .prompt({
+        type: "input",
+        name: "newdept",
+        message:"Enter name of new department."
+    })
+    .then((answer) => {
+        connnection.query(
+            "INSERT INTO department SET ?",
+        {
+            name: answer.newdept
+        },
+         (err, answer) => {
+             if (err) {
+                 throw err;
+             }
+         }
+        ),
+        console.table(answer);
+        questions();
+    });
+}
